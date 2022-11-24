d8bee08493aa5cf19e3769e5661c7d63fad92068
Tharsis <dobbinhuang@gmail.com>
2022-11-24 12:22:34 +0100

    including auto deletion of pdf
    30s timeout to wait for inscape to finish.
    start /w is not actually waiting, same problem with call

--- convert_pdf_to_svg.bat
+++ convert_pdf_to_svg.bat
@@ -1,2 +1,6 @@
 @echo off
-inkscape --pdf-page=1 --export-type=svg --export-area-drawing --export-filename C:\Users\dobbi\Documents\GitHub\tlk\terrain_table.svg "%~1"
+echo Converting PDF to SVG...
+START /B /WAIT inkscape --pdf-page=1 --export-type=svg --export-area-drawing --export-filename C:\Users\dobbi\Documents\GitHub\tlk\terrain_table.svg %1
+TIMEOUT /t 30
+echo deleting %1
+del %1
