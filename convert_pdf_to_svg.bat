ec6221a4c855559b677251839f0642f33df2699e
Tharsis <dobbinhuang@gmail.com>
2022-12-01 14:50:57 +0100

    Update convert_pdf_to_svg.bat

--- convert_pdf_to_svg.bat
+++ convert_pdf_to_svg.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Converting PDF to SVG...
 START /B /WAIT inkscape --pdf-page=1 --export-type=svg --export-area-drawing --export-filename C:\Users\dobbi\Documents\GitHub\tlk\terrain_table.svg %1
-TIMEOUT /t 30
+TIMEOUT /t 10
 echo deleting %1
 del %1
